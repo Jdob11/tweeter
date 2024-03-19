a833e29114cf2a8ed23391b46d969136c2a2c0a4
Jdob11 <jbdobson11@gmail.com>
2024-03-19 15:59:40 -0700

    chore: Lint JS file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,6 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+/* global $ */
 
 $(() => {
   const data = [
@@ -28,43 +29,43 @@ $(() => {
       },
       "created_at": 1461113959088
     }
-  ]
+  ];
 
-const renderTweets = (tweets) => {
-  const tweetArray = [];
-  const $tweetContainer = $('.tweet-container');
-  for (let tweet of tweets) {
-    tweet = createTweetElement(tweet);
-    tweetArray.push(tweet);
-  }
-  $tweetContainer.append(tweetArray);
-  return $tweetContainer
-}
+  const renderTweets = (tweets) => {
+    const tweetArray = [];
+    const $tweetContainer = $('.tweet-container');
+    for (let tweet of tweets) {
+      tweet = createTweetElement(tweet);
+      tweetArray.push(tweet);
+    }
+    $tweetContainer.append(tweetArray);
+    return $tweetContainer;
+  };
 
-const createTweetElement = (tweet) => {
-  const { user, content, created_at } = tweet;
-  const $tweet = $('<article>').addClass('tweet').addClass('tweet-border');
-  const $header = $('<header>');
-  const $avatarContainer = $('<div>').addClass('avatar-container');
-  const $avatarImage = $('<img>').addClass('avatar').attr('src', user.avatars).attr('alt', 'user avatar');
-  const $tweetUser = $('<p>').addClass('tweet-user').append($('<p>').text(user.name));
-  const $handle = $('<div>').addClass('handle').append($('<p>').text(user.handle));
-  const $tweetContent = $('<div>').addClass('tweet-content').append($('<p>').text(content.text));
-  const $footer = $('<footer>');
-  const $timeSinceTweet = $('<div>').addClass('time-since-tweet').append($('<p>').text(created_at));
-  const $tweetIcons = $('<div>').addClass('tweet-icons').append(
-    $('<i>').addClass('fa-solid fa-flag'),
-    $('<i>').addClass('fa-solid fa-retweet'),
-    $('<i>').addClass('fa-solid fa-heart'),
-  );
+  const createTweetElement = (tweet) => {
+    const { user, content, created_at: createdAt } = tweet;
+    const $tweet = $('<article>').addClass('tweet').addClass('tweet-border');
+    const $header = $('<header>');
+    const $avatarContainer = $('<div>').addClass('avatar-container');
+    const $avatarImage = $('<img>').addClass('avatar').attr('src', user.avatars).attr('alt', 'user avatar');
+    const $tweetUser = $('<p>').addClass('tweet-user').append($('<p>').text(user.name));
+    const $handle = $('<div>').addClass('handle').append($('<p>').text(user.handle));
+    const $tweetContent = $('<div>').addClass('tweet-content').append($('<p>').text(content.text));
+    const $footer = $('<footer>');
+    const $timeSinceTweet = $('<div>').addClass('time-since-tweet').append($('<p>').text(createdAt));
+    const $tweetIcons = $('<div>').addClass('tweet-icons').append(
+      $('<i>').addClass('fa-solid fa-flag'),
+      $('<i>').addClass('fa-solid fa-retweet'),
+      $('<i>').addClass('fa-solid fa-heart'),
+    );
 
-  $avatarContainer.append($avatarImage, $tweetUser);
-  $header.append($avatarContainer, $handle);
-  $footer.append($timeSinceTweet, $tweetIcons);
-  $tweet.append($header, $tweetContent, $footer);
+    $avatarContainer.append($avatarImage, $tweetUser);
+    $header.append($avatarContainer, $handle);
+    $footer.append($timeSinceTweet, $tweetIcons);
+    $tweet.append($header, $tweetContent, $footer);
 
-  return $tweet;
-};
+    return $tweet;
+  };
 
-renderTweets(data);
+  renderTweets(data);
 });
