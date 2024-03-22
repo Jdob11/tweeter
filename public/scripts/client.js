d0f90916955cc4b1c8f4499a60d0017d2b249793
Jdob11 <jbdobson11@gmail.com>
2024-03-22 11:38:07 -0700

    chore: Add comments to explain event listeners in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -29,6 +29,7 @@ $(() => {
     submitTweet();
   });
 
+  // event listener to reveal new tweet section when nav link is clicked, focus cursor in text area, and rehide new tweet section if clicked again
   $('.nav-links').on('click', '.nav-link, .fa-angles-down', function(event) {
     event.preventDefault();
     if ( $('.new-tweet').first().is(':hidden')) {
@@ -40,6 +41,7 @@ $(() => {
     }
   });
 
+  // event listener to hide nav link and reveal scroll up button when user has scrolled past 500px, to reverse if user scrolls back above 500px
   $(window).on('scroll', function() {
     if ($(this).scrollTop() < 500) {
       $('.scroll-up-button').slideUp('fast');
@@ -50,6 +52,7 @@ $(() => {
     }
   })
 
+  // event listener to scroll to the top, reveal the new tweet section and focus the cursor in the text area when scroll up button is clicked
   $('.scroll-up-button').on('click', function() {
     $('html, body').animate({ scrollTop: 0 }, 'slow');
     $('.new-tweet').slideDown('slow', function() {
